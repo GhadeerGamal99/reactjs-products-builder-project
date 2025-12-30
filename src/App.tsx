12f51e62b7440e06cb0ec7c1a364276da482655a
GhadeerGamal99 <ghadeer.gamal@luzedo.com>
2025-12-30 19:21:12 +0200

    custom modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+import { useState } from "react"
 import ProductCard from "./components/ProductCard"
+import Modal from "./components/ui/Modal"
 import { productList } from "./data"
+import Button from "./components/ui/Button"
 
 const App = () => {
+/* ___________STATE___________*/
+  let [isOpen, setIsOpen] = useState(false)
+
+  /* ___________RENDER___________*/
+  
+  function open() {
+    setIsOpen(true)
+  }
+
+  function close() {
+    setIsOpen(false)
+  }
+
   const renderProductList = productList.map(product => <ProductCard product={product} key={product.id} />)
   return (
-    <div>
-      <div className="m-5 p-2 rounded-md grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-2">
+    <div className="mx-auto max-w-5xl px-4">
+       <Button className="bg-red-700" onClick={open}>OPEN MODAL</Button>
+        <Modal isOpen={isOpen} closeModel={close}>
+           <div className="flex items-center justify-between space-x-2">
+                <Button className="bg-indigo-700">EDIT</Button>
+                <Button className="bg-red-700">DELETE</Button>
+
+            </div>
+
+        </Modal>
+      <div className="m-5 p-2 rounded-md grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-2 md:gap-4">
         {renderProductList}
       </div>
     </div>
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,34 +1,37 @@
 import type { IProduct } from "../interfaces"
+import { txtSlicer } from "../utils/functions"
 import Image from "./Image"
 import Button from "./ui/Button"
 
 interface IProps {
-product:IProduct
+    product: IProduct
 }
-const ProductCard = ({product}: IProps) => {
-    const {title,description,imageURL,price}=product;
+const ProductCard = ({ product }: IProps) => {
+    const { title, description, imageURL, price, category } = product;
     return (
-        <div className="flex flex-col border rounded-md p-2">
-           <Image className="rounded-md mb-2 max-w-full" imageURL={imageURL} alt="product image"></Image>
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <div className="flex my-4 space-x-2">
+        <div className="max-w-sm md:max-w-lg mx-auto md:mx-0 border rounded-md p-2 flex flex-col space-y-3">
+            <Image className="rounded-md h-52 w-full lg:object-cover" imageURL={imageURL} alt="product image"></Image>
+              <h3 className="text-md font-semibold">{txtSlicer(title, 20)}</h3>
+      <p className="text-sm text-gray-500 wrap-break-words">{txtSlicer(description)}</p>
+
+            <div className="flex items-center flex-wrap space-x-1">
                 <span className="w-5 h-5 rounded-full bg-indigo-600 cursor-pointer" />
                 <span className="w-5 h-5 rounded-full bg-red-600 cursor-pointer" />
                 <span className="w-5 h-5 rounded-full bg-yellow-600 cursor-pointer" />
             </div>
 
-            <div className="flex justify-between">
-                <span>{price}</span>
-                <Image className="w-10 h-10 rounded-full object-cover" imageURL="https://www.mytheresa.com/media/356/402/30/1e/P01102414_b2.jpg" alt="small product image" />
+            <div className="flex items-center justify-between">
+                    <span className="text-lg text-indigo-600 font-semibold"> $ {price}</span>
+                <div className="flex items-center space-x-2">
+                          <span className="text-xs font-semibold">{category.name}</span>
+                    <Image className="w-10 h-10 rounded-full object-cover" imageURL={category.imageURL} alt={category.name} />
+                </div>
             </div>
-            <div className="flex space-x-2 mt-5">
+            <div className="flex items-center justify-between space-x-2">
                 <Button className="bg-indigo-700">EDIT</Button>
                 <Button className="bg-red-700">DELETE</Button>
-                <Button className="bg-slate-900">LOADING</Button>
-                <Button className="bg-green-700">SUCCESS</Button>
-                <Button className="bg-gray-300">CANCEL</Button>
-</div>
+
+            </div>
 
         </div>
     )
