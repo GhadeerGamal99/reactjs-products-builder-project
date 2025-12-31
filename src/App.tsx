16efa8bd37b295d23461bad766e5abf31f25c2c2
GhadeerGamal99 <ghadeer.gamal@luzedo.com>
2025-12-31 14:33:56 +0200

    Error message toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { useState, type ChangeEvent } from "react"
+import { useState, type ChangeEvent, type FormEvent } from "react"
 import ProductCard from "./components/ProductCard"
 import Modal from "./components/ui/Modal"
 import { formInputsList, productList } from "./data"
 import Button from "./components/ui/Button"
 import Input from "./components/ui/Input"
 import type { IProduct } from "./interfaces"
+import { productValidation } from "./validation"
+import ErrorMessage from "./components/ui/ErrorMessage"
 
 const App = () => {
   /* ___________STATE___________*/
   const [isOpen, setIsOpen] = useState(false)
-  const [product, setProduct] = useState<IProduct>({
+  const [errors, setErrors] = useState( {  
+    title: '',
+    description: '',
+    price: '',
+    imageURL: '',})
+  const defaultProductObj:IProduct={
     title: '',
     description: '',
     price: '',
@@ -19,7 +26,8 @@ const App = () => {
       name: '',
       imageURL: '',
     }
-  })
+  }
+  const [product, setProduct] = useState<IProduct>(defaultProductObj)
 
   /* ___________RENDER___________*/
 
@@ -35,25 +43,53 @@ const App = () => {
 
     const { name, value } = event.target;
     setProduct({ ...product, [name]: value })
+    setErrors({...errors,[name]:""})
   }
+  
+  const onCancelHandler= ()=>{
+    setProduct(defaultProductObj)
+    setIsOpen(false)
+  }
+  const onSubmitHandler=(event: FormEvent<HTMLFormElement>): void=> {
+  event.preventDefault();
+
+  const {title,description,price,imageURL}=product;
+  const errors=productValidation({
+    title,
+    description,
+    price,
+    imageURL,
+  })
+  const hasErrorMsg=Object.values(errors).some(value=> value=='')&&Object.values(errors).every(value=> value=='') ;
+  console.log(hasErrorMsg)
+
+  if(!hasErrorMsg){
+    setErrors(errors)
+   return;
+  }
+  console.log("SEND THIS PRODUCT TO SERVER ")
+}
 
   const renderProductList = productList.map(product => <ProductCard product={product} key={product.id} />)
   const renderFormInputList = formInputsList.map(input =>
-    <div className="flex flex-col">
+    <div className="flex flex-col" key={input.id}>
       <label className="text-sm font-medium mb text-gray-700" htmlFor={input.id}>{input.label}</label>
       <Input type="text" name={input.name} id={input.id} value={product[input.name]} onChange={onChangeHandler} />
+      <ErrorMessage msg={errors[input.name]}/>
     </div>)
+
+
 
   return (
     <div className="mx-auto max-w-5xl px-4">
       <Button className="bg-red-700" onClick={open}>OPEN MODAL</Button>
 
       <Modal isOpen={isOpen} closeModel={close} title="ADD A NEW PRODUCT">
-        <form className="space-y-3">
+        <form className="space-y-3" onSubmit={onSubmitHandler}>
           {renderFormInputList}
           <div className="flex items-center justify-between space-x-3">
-            <Button className="bg-indigo-700 hover:bg-indigo-800">submit</Button>
-            <Button className="bg-gray-400 hover:bg-gray-500">Cancel</Button>
+            <Button className="bg-indigo-700 hover:bg-indigo-800" >submit</Button>
+            <Button className="bg-gray-400 hover:bg-gray-500" onClick={onCancelHandler}>Cancel</Button>
 
           </div>
         </form>
---
+++ src/components/ui/ErrorMessage.tsx
@@ -0,0 +1,8 @@
+interface IProps {
+msg:string;
+}
+const ErrorMessage = ({msg}: IProps) => {
+  return msg?<span className="block text-sm text-red-700 font-semibold">{msg}</span>:null;
+}
+
+export default ErrorMessage
