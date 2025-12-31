cac9fac54dd16fe08528bcf888885607540873d1
GhadeerGamal99 <ghadeer.gamal@luzedo.com>
2025-12-31 12:28:59 +0200

    product state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { useState } from "react"
+import { useState, type ChangeEvent } from "react"
 import ProductCard from "./components/ProductCard"
 import Modal from "./components/ui/Modal"
 import { formInputsList, productList } from "./data"
 import Button from "./components/ui/Button"
 import Input from "./components/ui/Input"
+import type { IProduct } from "./interfaces"
 
 const App = () => {
   /* ___________STATE___________*/
-  let [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
+ const [product,setProduct]=useState<IProduct>({
+title:'',
+description:'',
+price:'',
+imageURL:'', 
+ colors: [],
+  category: {
+    name: '',
+    imageURL: '',
+  } 
+ })
 
   /* ___________RENDER___________*/
 
@@ -19,15 +31,23 @@ const App = () => {
     setIsOpen(false)
   }
 
+  const onChangeHandler=(event:ChangeEvent<HTMLInputElement>)=>{
+
+    const {name,value}=event.target;
+    setProduct({...product,[name]:value})
+  }
+
   const renderProductList = productList.map(product => <ProductCard product={product} key={product.id} />)
   const renderFormInputList = formInputsList.map(input =>
     <div className="flex flex-col">
       <label className="text-sm font-medium mb text-gray-700" htmlFor={input.id}>{input.label}</label>
-      <Input type="text" name={input.name} id={input.id} />
+      <Input type="text" name={input.name} id={input.id} value={''} onChange={onChangeHandler}/>
     </div>)
+
   return (
     <div className="mx-auto max-w-5xl px-4">
       <Button className="bg-red-700" onClick={open}>OPEN MODAL</Button>
+
       <Modal isOpen={isOpen} closeModel={close} title="ADD A NEW PRODUCT">
         <form className="space-y-3">
           {renderFormInputList}
@@ -37,8 +57,8 @@ const App = () => {
 
           </div>
         </form>
-
       </Modal>
+
       <div className="m-5 p-2 rounded-md grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3
        xl:grid-cols-4  gap-2 md:gap-4">
         {renderProductList}
